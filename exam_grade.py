bb898da1e7700b81418926b4ad45445dde649f29
Ramdhan Firdaus Amelia <70691224+ramdhanfirdaus@users.noreply.github.com>
2023-02-28 10:07:34 +0700

    “Update

--- exam_grade.py
+++ exam_grade.py
@@ -1,7 +1,7 @@
 def exam_grade(score):
     if score > 95:
         grade = "Top Score"
-    elif score < 60:
+    elif score >= 60:
         grade = "Pass"
     else:
         grade = "Fail"
@@ -10,3 +10,4 @@ def exam_grade(score):
 
 print(exam_grade(55))  # Should be Fail
 print(exam_grade(60))  # Should be Pass
+print(exam_grade(100))  # Should be Top score
